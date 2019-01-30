29c37bac0209a9b575760522045d1e97e9bb401e
V4stg <lukacsb@gmail.com>
2019-01-31 00:00:01 +0100

    implemented Timer

--- TimerEvents/MainWindow.xaml.cs
+++ TimerEvents/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimerEvents
 {
@@ -20,9 +9,28 @@ namespace TimerEvents
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Tick += new EventHandler(dispathcerTimer_Tick);
+            _timer.Start();
+        }
+
+        private void dispathcerTimer_Tick(object sender, EventArgs e)
+        {
+            myProgressBar.Value += 10;
+            if (myProgressBar.Value >= 100)
+            {
+                _timer.Stop();
+            }
         }
     }
 }
